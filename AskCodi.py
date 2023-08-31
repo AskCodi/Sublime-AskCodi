b9bdf22c7f29618ca384503ec98a6b7ec3ee515a
assistivai <assistivai@gmail.com>
2023-08-31 15:41:33 +0530

    version 4.1

--- AskCodi.py
+++ AskCodi.py
@@ -10,24 +10,105 @@
 
 import sublime
 import sublime_plugin
-import webbrowser
-import uuid
-import time
 import threading
 from urllib import request
 import urllib
 import json
-import platform
+import mdpopups
+import textwrap
 
 
 # globals
 EDITOR = "Sublime"
-CODI_VERSION = "4.0"
+CODI_VERSION = "4.1"
 ST_VERSION = int(sublime.version())
 SETTINGS_FILE = 'AskCodi.sublime-settings'
 SETTINGS = {}
-API_KEY = ""
 URL = "https://app.askcodi.com/api/askcodi-extension/"
+
+class Constants:
+    api_key = '' 
+    code = ""
+
+CSS_CLASS_NAME = "askcodi-popup"
+CSS = """
+    html {{
+        --askcodi-accept-foreground: var(--foreground);
+        --askcodi-accept-background: var(--background);
+        --askcodi-accept-border: var(--purplish);
+        --askcodi-reject-foreground: var(--foreground);
+        --askcodi-reject-background: var(--background);
+        --askcodi-reject-border: var(--yellowish);
+        --askcodi-copy-foreground: var(--foreground);
+        --askcodi-copy-background: var(--background);
+        --askcodi-copy-border: var(--greenish);
+    }}
+
+    .{class_name} {{
+        margin: 1rem 0.5rem 0 0.5rem;
+    }}
+
+    .{class_name} .header {{
+        display: block;
+        margin-bottom: 1rem;
+    }}
+
+    .{class_name} a {{
+        border-radius: 3px;
+        border-style: solid;
+        border-width: 1px;
+        display: inline;
+        padding: 5;
+        text-decoration: none;
+    }}
+
+    .{class_name} a.accept {{
+        background: var(--askcodi-accept-background);
+        border-color: var(--askcodi-accept-border);
+        color: var(--askcodi-accept-foreground);
+    }}
+
+    .{class_name} a.accept i {{
+        color: var(--askcodi-accept-border);
+    }}
+
+    .{class_name} a.reject {{
+        background: var(--askcodi-reject-background);
+        border-color: var(--askcodi-reject-border);
+        color: var(--askcodi-reject-foreground);
+    }}
+
+    .{class_name} a.reject i {{
+        color: var(--askcodi-reject-border);
+    }}
+
+    .{class_name} a.copy {{
+        background: var(--askcodi-copy-background);
+        border-color: var(--askcodi-copy-border);
+        color: var(--askcodi-copy-foreground);
+    }}
+
+    .{class_name} a.copy i {{
+        color: var(--askcodi-copy-border);
+    }}
+    """.format(
+        class_name=CSS_CLASS_NAME
+    )
+
+COMPLETION_TEMPLATE = textwrap.dedent(
+        """
+        <div class="header">{header_items}</div>
+        ``````
+        {code}
+        ``````
+        """
+    )
+
+header_items = [
+                    '<a class="accept" title="Insert Code" href="subl:insert_code"><i>✓</i> Insert</a>',
+                    '<a class="reject" title="Reject Code" href="subl:reject_code"><i>×</i> Reject</a>',
+                    '<a class="copy" title="Copy Code" href="subl:copy_code"><i>♢</i> Copy</a>',
+                    ]
 
 def update_status_bar(message):
     try:
@@ -61,22 +142,22 @@ class ApiKey(object):
         sublime.save_settings(SETTINGS_FILE)
 
 
-APIKEY = ApiKey()
+Constants.api_key = ApiKey()
 
 
 def prompt_api_key():
-    # if APIKEY.read():
-    #     return True
+    if Constants.api_key.read():
+        return True
 
     window = sublime.active_window()
     if window:
         def got_key(text):
-            if text:
-                APIKEY.write(text)
-        window.show_input_panel('[AskCodi] Enter your AskCodi api key:', '', got_key, None, None)
+            # if text:
+                Constants.api_key.write(text)
+        window.show_input_panel('[AskCodi]: Enter your AskCodi api key:', '', got_key, None, None)
         return True
     else:
-        log(ERROR, 'Could not prompt for api key because no window found.')
+        log(ERROR, '[AskCodi]: Could not prompt for api key because no window found.')
         return False
 
 
@@ -95,12 +176,13 @@ def plugin_loaded():
         SETTINGS.set('explain_code', True)
         SETTINGS.set('test_code', True)
         SETTINGS.set('document_code', True)
+        SETTINGS.set('complete_code', True)
         SETTINGS.set('context', True)
         sublime.save_settings(SETTINGS_FILE)
     
     SETTINGS = sublime.load_settings(SETTINGS_FILE)
 
-    update_status_bar('Initializing AskCodi...')
+    update_status_bar('[AskCodi]: Initializing AskCodi...')
     if not prompt_api_key():
         set_timeout(after_loaded, 0.5)
 
@@ -111,11 +193,11 @@ def ask_codi_api(app, query, context, self, edit):
     try:
         # if not prompt_api_key():
         #     set_timeout(after_loaded, 0.5)
-        update_status_bar("Asking Codi...")
+        update_status_bar("[AskCodi]: Asking Codi...")
         try:
             language = "." + self.view.window().active_view().file_name().split(".")[-1]
         except:
-            update_status_bar("Please save the file with a proper language extension first to use AskCodi.")
+            update_status_bar("[AskCodi]: Please save the file with a proper language extension first to use AskCodi.")
         headers = {
             'apikey': SETTINGS.get("api_key"),
             'source': EDITOR,
@@ -137,32 +219,42 @@ def ask_codi_api(app, query, context, self, edit):
         try:
             req = request.Request(URL, data=data, headers=headers)
             response = request.urlopen(req).read().decode('utf-8')
-            print(response)
         
             # parse json response
             resp = json.loads(response)
-            print(resp)
-            self.panel = self.view.window().create_output_panel('AskCodi')
-            self.panel.run_command('insert', {'characters': str(response)})
-            self.view.window().run_command('show_panel', {'panel': 'output.AskCodi'})
-            update_status_bar("See suggestion from Codi")
-            sublime.status_message('See suggestion from Codi')
 
             if (resp["status"]):
-                self.panel = self.view.window().create_output_panel('AskCodi')
-                self.panel.run_command('insert', {'characters': resp["message"]})
-                self.view.window().run_command('show_panel', {'panel': 'output.AskCodi'})
-                update_status_bar("See suggestion from Codi")
-                sublime.status_message('See suggestion from Codi')
+                content = COMPLETION_TEMPLATE.format(
+                        header_items=" &nbsp;".join(header_items),
+                        code=textwrap.dedent(resp["message"]),
+                        )
+
+                mdpopups.show_popup(
+                    view=self.view,
+                    content=content,
+                    md=True,
+                    css=CSS,
+                    layout=sublime.LAYOUT_INLINE,
+                    flags=sublime.COOPERATE_WITH_AUTO_COMPLETE,
+                    max_width=640,
+                    max_height=640,
+                    wrapper_class=CSS_CLASS_NAME,
+                    )
+
+                Constants.code = textwrap.dedent(resp["message"])
+                if len(Constants.code.split("```")) > 1:
+                    Constants.code= Constants.code.split("```")[1]
+                update_status_bar("[AskCodi]: See suggestion from Codi")
             else:
                 update_status_bar("AskCodi: " + resp["message"])
         except urllib.error.HTTPError as e:
-            update_status_bar("AskCodi: " + json.loads(e.read().decode('utf-8'))["message"])
+            update_status_bar("[AskCodi]: " + json.loads(e.read().decode('utf-8'))["message"])
             if e.code == 401:
                 prompt_api_key()
 
     except Exception as e:
-        print(e)
+        # print(e)
+        # update_status_bar(str(e))
         update_status_bar("AskCodi: Couldn't complete request.")
         pass
 
@@ -174,17 +266,17 @@ class GenerateCodeCommand(sublime_plugin.TextCommand):
             selected = self.view.substr(sel)
             context = ""
             if len(selected) > 0:
-                for region in self.view.sel():
-                    region.begin()
-                    context = self.view.substr(sublime.Region(region.begin() - 512, region.begin()))
-                # MakeRequest(self.view, "Generate", query, context).start()
+                if(SETTINGS.get("context")):
+                    for region in self.view.sel():
+                        region.begin()
+                        context = self.view.substr(sublime.Region(region.begin() - 1024, region.begin()))
                 gc_thread = threading.Thread(target=ask_codi_api,
                                             args=["Generate", selected, context.strip(), self, edit])
                 gc_thread.start()
             else:
-                update_status_bar("Please select your query.")
+                update_status_bar("[AskCodi]: Please select your query.")
         else:
-            update_status_bar("Please enable generate code from settings...")
+            update_status_bar("[AskCodi]: Please enable generate code from settings...")
 
 
 class ExplainCodeCommand(sublime_plugin.TextCommand):
@@ -197,9 +289,9 @@ class ExplainCodeCommand(sublime_plugin.TextCommand):
                                                     args=["Explain", selected, "", self, edit])
                 ec_thread.start()
             else:
-                update_status_bar("Please select your query.")
+                update_status_bar("[AskCodi]: Please select your query.")
         else:
-            update_status_bar("Please check user settings for AskCodi for explain_code setting.")
+            update_status_bar("[AskCodi]: Please check user settings for AskCodi for explain_code setting.")
 
 
 class DocumentCodeCommand(sublime_plugin.TextCommand):
@@ -212,9 +304,9 @@ class DocumentCodeCommand(sublime_plugin.TextCommand):
                                             args=["Document", selected, "", self, edit])
                 dc_thread.start()
             else:
-                update_status_bar("Please select your query.")
+                update_status_bar("[AskCodi]: Please select your query.")
         else:
-            update_status_bar("Please check user settings for AskCodi for document_code setting.")
+            update_status_bar("[AskCodi]: Please check user settings for AskCodi for document_code setting.")
 
 
 class TestCodeCommand(sublime_plugin.TextCommand):
@@ -228,9 +320,52 @@ class TestCodeCommand(sublime_plugin.TextCommand):
                                             args=["Test", selected, context, self, edit])
                 tc_thread.start()
             else:
-                update_status_bar("Please select your query.")
+                update_status_bar("[AskCodi]: Please select your query.")
         else:
-            update_status_bar("Please check user settings for AskCodi for test_code setting.")
+            update_status_bar("[AskCodi]: Please check user settings for AskCodi for test_code setting.")
+
+
+
+class CompleteCodeCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        if SETTINGS.get("complete_code") and SETTINGS.get("context"):
+            sel = self.view.sel()[0]
+            selected = self.view.substr(sel)
+            context = ""
+            for region in self.view.sel():
+                    region.begin()
+                    context = self.view.substr(sublime.Region(region.begin() - 1024, region.begin()))
+            tc_thread = threading.Thread(target=ask_codi_api,
+                                        args=["Complete", context.strip(), "", self, edit])
+            tc_thread.start()
+        else:
+            update_status_bar("[AskCodi]: Please check user settings for AskCodi for complete_code setting.")
+
+
+class InsertCodeCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        mdpopups.hide_popup(self.view)
+        source_line_region = self.view.line(sublime.Region(*self.view.sel()[0].to_tuple()))
+        self.view.insert(edit, source_line_region.end(), Constants.code)
+        self.view.show(self.view.sel(), show_surrounds=False, animate=True)
+
+        update_status_bar("[AskCodi]: code inserted")
+
+
+class RejectCodeCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        mdpopups.hide_popup(self.view)
+        Constants.code = ""
+        update_status_bar("[AskCodi]: code rejected.")
+
+
+class CopyCodeCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        mdpopups.hide_popup(self.view)
+        sublime.set_clipboard(Constants.code)
+        Constants.code = ""
+        update_status_bar("[AskCodi]: code copied.")
+
 
 
 # plugin_loaded is called manually for sublime text version<3
--- README.md
+++ README.md
@@ -1,11 +1,10 @@
 ## Accelerate Your Development Workflow With AskCodi
 **[AskCodi](https://askcodi.com)** is an AI code assistant developed by Assistiv.ai, that **facilitates coders** to code faster by providing following features:<br/>
-**1. Auto-complete (only in VS Code),**<br/>
 **2. Code suggestions for natural language (Ask AI to code),**<br/>
 **3. Time complexity for your code,**<br/>
 **4. Document Code.**<br/>
 **5. Explain Code,**<br/>
-**6. Complete Code (Manually triggerd, works same as auto-complete),**<br/>
+**6. Complete Code,**<br/>
 **7. Test Code.**<br/>
 <br/>
 Whether you are new to coding or a professional, working individually or in a team, AskCodi helps save time by answering your queries in your favourite IDE.
@@ -24,9 +23,9 @@ With the support of teams, you can accelerate the process by sharing suggestions
 
 4. Type `askcodi`, then press `enter` with the `AskCodi` plugin selected.
 
-5. Follow the sign-in process to authenticate your device..
+5. AskCodi will prompt to add an API key.
 
-6. Use Sublime and Codi will be ready to pair program with you. Your code generated will be in your codebase that can be explored in [Webapp](https://app.askcodi.com).
+6. Go to[Webapp](https://app.askcodi.com), sign in and in your account settings, copy the integrations key in Sublime Text.
 
 
 ## Usage
@@ -37,16 +36,16 @@ With the support of teams, you can accelerate the process by sharing suggestions
 
 **3. Test Code:** Select the code, right click and go to AskCodi. Click on Test Code and wait for bottom panel to open and display the results.
 
-**3. Complete Code:** Complete code can be triggered using hot keys `ctrl + alt + 1` or `ctrl + option + 1` only. Whether you are writing a statement or code, complete code can be triggered directly using the hot key to complete the code or even comments.
+**3. Complete Code:** Complete code can be triggered using hot keys `ctrl + alt + ]` or `ctrl + option + ]` , or right click and go to AskCodi. Whether you are writing a statement or code, complete code can be triggered directly using the hot key to complete the code or even comments.
 
 **4. Explain Code:** Select the code, right click and go to AskCodi. Click on Explain Code and a bottom dialogue box opens. Keep the default text as it is to get the time complexity of the selected code. Delete and either provide a different information if you want(like a library)or leave it empty, and hit enter to get code explanation. Wait for bottom panel to open and display the results.
 
 
 ## How it works
 
-1. AskCodi uses OpenAI GPT3 to generate suggestions.
+1. AskCodi uses OpenAI GPT3.5 Turbo and GPT-4(coming soon) to generate suggestions.
 
-2. For the **context, Codi uses 512 characters before the selected query, or position of cursor/caret and uses that code to provide relavant suggestions. You can turn off the context from the user settings file (see section "Change user settings") by changing from "true" to "false"** Please note, AskCodi does not save this context, however it is recommended that you do not include any sensitive data in the context or query. Also, with context "off", **Complete code** application won't work.
+2. For the **context, Codi uses 1024 characters before the selected query, or position of cursor/caret and uses that code to provide relavant suggestions. You can turn off the context from the user settings file (see section "Change user settings") by changing from "true" to "false"** Please note, AskCodi does not save this context, however it is recommended that you do not include any sensitive data in the context or query. Also, with context "off", **Complete code** application won't work.
 
 3. AskCodi sends the context code(if true in settings) and the selected text/code to the servers to generate suggestions.
 
